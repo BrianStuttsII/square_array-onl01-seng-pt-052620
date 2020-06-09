9fc2f450684a2d89932c016a76e339d9c997cf6c
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-09 05:04:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,25 @@
 def square_array(numbers)
-  
 
+numbers_new = []
  
-  numbers_new = []
- 
- numbers.each do |num|
-  numbers_new << (num ** 2)
+
+   numbers.each do |num|
+numbers_new << (num ** 2)
+
  end
  return numbers_new
 end
 
 
 
+<<<<<<< HEAD
+=======
+# def square_array(numbers)
+# new numbers []
+ 
+ # numbers.each do |num|
+   # new_numbers << (num ** 2)
+#  end
+ # return new_numbers
+# end
+>>>>>>> ea3bec535108add83f350cc533624be7d5ca473f
